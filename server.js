45a176e87602155b3f3abf61c0c3c57b4669c315
robyn <robyn3choi@gmail.com>
2019-03-26 17:04:39 -0700

    finished but put in dummy calls for now

--- server.js
+++ server.js
@@ -2,19 +2,35 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const fetch = require('node-fetch');
+const rateLimit = require('express-rate-limit');
+const nodeGeocoder = require('node-geocoder');
 
 require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
-app.use(cors()); 
+app.use(cors());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 min
+  max: 30 // limit each IP to 30 requests per windowMs
+});
+//  apply to all requests
+app.use(limiter);
 
-app.get('/currentweather', (req, res) => {
-  getCurrentWeatherFromPos(req.query.lat, req.query.long).then(result => res.send(result));
+var geocoder = nodeGeocoder({
+  provider: 'opencage',
+  apiKey: process.env.OPENCAGE_SECRET
 });
 
-const getCurrentWeatherFromPos = (lat, long) => {
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+const getCurrentWeather = (lat, long) => {
+  //TODO REMOVE
+  return Promise.resolve({
+    description: 'Partly cloudy',
+    icon: 'cloudy',
+    temperature: 12
+  });
   const url = `https://api.darksky.net/forecast/${process.env.DARKSKY_SECRET}/${lat},${long}?units=auto`;
   return fetch(url)
     .then(response => response.json())
@@ -24,14 +40,102 @@ const getCurrentWeatherFromPos = (lat, long) => {
         icon: json.currently.icon,
         temperature: Math.round(json.currently.temperature)
       }
-    });
+    })
+    .catch(err => console.log(err));
 }
 
-app.get('/forecast', (req, res) => {
-  getForecastFromPos(req.query.lat, req.query.long).then(result => res.send(result));
-});
-
-const getForecastFromPos = (lat, long) => {
+const getForecast = (lat, long) => {
+  //TODO REMOVE
+  return Promise.resolve([
+    {
+      weekday: 0,
+      month: 0,
+      date: 20,
+      description: 'Partly cloudy throughout the day.',
+      icon: 'fog',
+      tempHigh: 20,
+      tempLow: 2,
+      wind: 10,
+      precipChance: 100
+    },
+    {
+      weekday: 1,
+      month: 0,
+      date: 20,
+      description: 'Partly cloudy throughout the day.',
+      icon: 'hail',
+      tempHigh: 20,
+      tempLow: 2,
+      wind: 10,
+      precipChance: 100
+    },
+    {
+      weekday: 2,
+      month: 0,
+      date: 20,
+      description: 'Partly cloudy throughout the day.',
+      icon: 'sleet',
+      tempHigh: 20,
+      tempLow: 2,
+      wind: 10,
+      precipChance: 100
+    },
+    {
+      weekday: 3,
+      month: 0,
+      date: 20,
+      description: 'Partly cloudy throughout the day.',
+      icon: 'snow',
+      tempHigh: 20,
+      tempLow: 2,
+      wind: 10,
+      precipChance: 100
+    },
+    {
+      weekday: 4,
+      month: 0,
+      date: 20,
+      description: 'Partly cloudy throughout the day.',
+      icon: 'thunderstorm',
+      tempHigh: 20,
+      tempLow: 2,
+      wind: 10,
+      precipChance: 100
+    },
+    {
+      weekday: 5,
+      month: 0,
+      date: 20,
+      description: 'Partly cloudy throughout the day.',
+      icon: 'tornado',
+      tempHigh: 20,
+      tempLow: 2,
+      wind: 10,
+      precipChance: 100
+    },
+    {
+      weekday: 6,
+      month: 0,
+      date: 20,
+      description: 'Partly cloudy throughout the day.',
+      icon: 'wind',
+      tempHigh: 20,
+      tempLow: 2,
+      wind: 10,
+      precipChance: 100
+    },
+    {
+      weekday: 0,
+      month: 0,
+      date: 20,
+      description: 'Partly cloudy throughout the day.',
+      icon: 'wind',
+      tempHigh: 20,
+      tempLow: 2,
+      wind: 10,
+      precipChance: 100
+    },
+  ]);
   const url = `https://api.darksky.net/forecast/${process.env.DARKSKY_SECRET}/${lat},${long}?units=auto&exclude=currently,minutely,hourly,alerts,flags`;
   return fetch(url)
     .then(response => response.json())
@@ -44,15 +148,62 @@ const getForecastFromPos = (lat, long) => {
           date: date.getDate(),
           description: day.summary,
           icon: day.icon,
-          tempHigh:  Math.round(day.temperatureHigh),
-          tempLow:  Math.round(day.temperatureLow),
-          wind:  Math.round(day.windSpeed),
-          precipChance: Math.roung(day.precipProbability * 100)
+          tempHigh: Math.round(day.temperatureHigh),
+          tempLow: Math.round(day.temperatureLow),
+          wind: Math.round(day.windSpeed),
+          precipChance: Math.round(day.precipProbability * 100)
         };
       })
+    })
+    .catch(err => console.log(err));
+}
+
+// get weather data from city search
+app.get('/citysearch', (req, res) => {
+  getCoordinates(req.query.placename)
+    .then(coords => getCurrentWeatherAndForecast(coords))
+    .then(result => res.send(result))
+    .catch(err => console.log(err));
+});
+
+const getCoordinates = (placename) => {
+  //TODO REMOVE
+  return Promise.resolve({ lat: 100, long: 100 });
+  return geocoder.geocode(placename)
+    .then(res => {
+      return { lat: res[0].latitude, long: res[0].longitude };
+    })
+    .catch(err => console.log(err));
+}
+
+const getCurrentWeatherAndForecast = (coords) => {
+  const currentWeather = getCurrentWeather(coords.lat, coords.long);
+  const forecast = getForecast(coords.lat, coords.long);
+
+  return Promise.all([currentWeather, forecast])
+    .then(results => {
+      return { currentWeather: results[0], forecast: results[1] };
     });
 }
 
-app.listen(process.env.PORT, ()=> {
+// get weather data and place name from position
+app.get('/weather', (req, res) => {
+  const coords = { lat: req.query.lat, long: req.query.long };
+
+  const placeName = geocoder.geocode(`${coords.lat}, ${coords.long}`)
+    .then(res => res[0].city + ', ' + res[0].state);
+
+  const weatherData = getCurrentWeatherAndForecast(coords)
+    .then(result => result);
+
+  Promise.all([placeName, weatherData])
+    .then(results => {
+      results[1].placeName = results[0];
+      res.send(results[1]);
+    });
+});
+
+
+app.listen(process.env.PORT, () => {
   console.log(`app is running on port ${process.env.PORT}`);
 })
