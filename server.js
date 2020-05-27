6ff57482b078f05a76180fb95aa795b711fd9b12
Robyn Choi <robyn3choi@gmail.com>
2020-05-26 20:14:56 -0700

    set up app for google app engine

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.enable("trust proxy"); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+app.enable('trust proxy'); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 min
-  max: 30 // limit each IP to 30 requests per windowMs
+  max: 30, // limit each IP to 30 requests per windowMs
 });
 //  apply to all requests
 app.use(limiter);
@@ -24,31 +24,31 @@ app.get('/', (req, res) => {
 
 var geocoder = nodeGeocoder({
   provider: 'opencage',
-  apiKey: process.env.OPENCAGE_SECRET
+  apiKey: process.env.OPENCAGE_SECRET,
 });
 
 const getWeatherDataFromPosition = (lat, long) => {
   const url = `https://api.darksky.net/forecast/${process.env.DARKSKY_SECRET}/${lat},${long}?units=auto&exclude=minutely,hourly,alerts`;
   return fetch(url)
-    .then(response => response.json())
-    .then(json => {
+    .then((response) => response.json())
+    .then((json) => {
       const forecast = assembleForecast(json.daily.data);
 
       const currentWeather = {
         description: json.currently.summary,
         icon: json.currently.icon,
         temperature: Math.round(json.currently.temperature),
-        units: json.flags.units
-      }
+        units: json.flags.units,
+      };
 
       return {
         currentWeather: currentWeather,
         forecast: forecast,
-        units: json.flags.units
-      }
+        units: json.flags.units,
+      };
     })
-    .catch(err => console.log(err));
-}
+    .catch((err) => console.log(err));
+};
 
 const assembleForecast = (data) => {
   return data.map((day, i) => {
@@ -62,46 +62,48 @@ const assembleForecast = (data) => {
       tempHigh: Math.round(day.temperatureHigh),
       tempLow: Math.round(day.temperatureLow),
       wind: Math.round(day.windSpeed),
-      precipChance: Math.round(day.precipProbability * 100)
+      precipChance: Math.round(day.precipProbability * 100),
     };
   });
-}
+};
 
 // get weather data from city search
 app.get('/search', (req, res) => {
   getCoordinates(req.query.placename)
-    .then(coords => getWeatherDataFromPosition(coords.lat, coords.long))
-    .then(result => res.send(result))
-    .catch(err => console.log(err));
+    .then((coords) => getWeatherDataFromPosition(coords.lat, coords.long))
+    .then((result) => res.send(result))
+    .catch((err) => console.log(err));
 });
 
 const getCoordinates = (placename) => {
-  return geocoder.geocode(placename)
-    .then(res => {
+  return geocoder
+    .geocode(placename)
+    .then((res) => {
       return { lat: res[0].latitude, long: res[0].longitude };
     })
-    .catch(err => console.log(err));
-}
+    .catch((err) => console.log(err));
+};
 
 // get weather data and place name from position
 app.get('/weather', (req, res) => {
-  const placeName = geocoder.geocode(`${req.query.lat}, ${req.query.long}`)
-    .then(res => res[0].city + ', ' + res[0].state)
-    .catch(err => console.log(err));
+  const placeName = geocoder
+    .geocode(`${req.query.lat}, ${req.query.long}`)
+    .then((res) => res[0].city + ', ' + res[0].state)
+    .catch((err) => console.log(err));
 
   const weatherData = getWeatherDataFromPosition(req.query.lat, req.query.long)
-    .then(result => result)
-    .catch(err => console.log(err));
+    .then((result) => result)
+    .catch((err) => console.log(err));
 
   Promise.all([placeName, weatherData])
-    .then(results => {
+    .then((results) => {
       results[1].placeName = results[0];
       res.send(results[1]);
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
-
-app.listen(3000, () => {
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
   console.log(`app is running on port 3000`);
-})
+});
